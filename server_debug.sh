dd75b1ddffaa62c33f6b4317bb171985394a8cf7
“shihaiyang” <haiyang@sph.com.sg>
2024-07-11 11:05:50 +0800

    增加debug

--- server_debug.sh
+++ server_debug.sh
@@ -1,2 +1,2 @@
 cd lib
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5006  -jar ./jpt-ui-backend-0.2-SNAPSHOT.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006  -jar ./jpt-ui-backend-0.2-SNAPSHOT.jar
