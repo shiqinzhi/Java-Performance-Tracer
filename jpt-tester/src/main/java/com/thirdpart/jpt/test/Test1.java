a1811cb0e19d4fda8c6351a4d77c7656a7ac7338
“shihaiyang” <haiyang@sph.com.sg>
2024-07-11 11:15:37 +0800

    调整测试用例

--- jpt-tester/src/main/java/com/thirdpart/jpt/test/Test1.java
+++ jpt-tester/src/main/java/com/thirdpart/jpt/test/Test1.java
@@ -7,16 +7,18 @@ import java.util.Random;
  */
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("这是在第一层");
-        try {
-            Thread.sleep(500+new Random().nextInt(100));
-        } catch (Exception e) {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("这是在第一层");
+            try {
+                Thread.sleep(500 + new Random().nextInt(100));
+            } catch (Exception e) {
 
+            }
+            new Test2().test2();
         }
-        new Test2().test2();
         try {
             System.out.println("等====关闭===");
-            Thread.sleep(3000+new Random().nextInt(100));
+            Thread.sleep(3000 + new Random().nextInt(100));
         } catch (Exception e) {
 
         }
--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ export jpt_client_log=true
 cd lib
 java -javaagent:./jpt-agent-0.2-SNAPSHOT.jar \
       -cp ./jpt-collector-0.2-SNAPSHOT.jar:./jpt-tester-0.2-SNAPSHOT.jar \
-      com.thirdpart.jpt.test.JPT_Tester_Application
+      com.thirdpart.jpt.test.Test1
